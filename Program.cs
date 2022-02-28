f7c9a043799b3397bb4e7a83ce6c4eb39e944280
Nathan Center <nathanjcenter@gmail.com>
2022-02-28 13:18:10 -0600

    added phase 5

--- Program.cs
+++ Program.cs
@@ -1,27 +1,36 @@
 ﻿using System;
 
+//phase 5
+int secretNumber=new Random().Next(1,100);
 
-//Console.WriteLine($" your guess was: {guess}");
 
 
-int secretNumber=42;
 // phase 3
 int tries=4;
 int guessNumber=1;
 while (guessNumber<=tries){
     //phase 4
-    Console.WriteLine($"Your guess - {guessNumber}");
+    Console.WriteLine($"Your guess  {guessNumber} out of 4");
+   
     //phase   1
 
-    Console.WriteLine("User needs to guess the right number");
+    Console.WriteLine("Please guess the secret number");
 int guess=int.Parse(Console.ReadLine());
 //phase 2
 if (guess==secretNumber){
     Console.WriteLine("You guess right!");
     break;
 }
+// phase 5
 else {
-    Console.WriteLine("Sorry you are wrong!");
+    Console.WriteLine("Sorry guessed wrong!");
+     if(tries-guessNumber==1)
+     {  
+         Console.WriteLine($"You have 1 guess remining");
+     
+     } else {
+        Console.WriteLine($"You have {tries-guessNumber} tries");
+     }
     
 }
 //phase 4
