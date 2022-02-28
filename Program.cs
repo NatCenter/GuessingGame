55f40cdf1c6cbda5ae2556a8d497fa63c171dff7
Nathan Center <nathanjcenter@gmail.com>
2022-02-28 10:56:04 -0600

    added phase 2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
 ﻿using System;
-//
-Console.WriteLine("User needs to input a number");
-string guess=Console.ReadLine();
-Console.WriteLine($" your guess was: {guess}");
+//phase 1
+Console.WriteLine("User needs to guess the right number");
+int guess=int.Parse(Console.ReadLine());
+//Console.WriteLine($" your guess was: {guess}");
+//phase 2
+
+int secretNumber=42;
+
+if (guess==secretNumber){
+    Console.WriteLine("You guess right!");
+}
+else {
+    Console.WriteLine("Sorry you are wrong!");
+}
