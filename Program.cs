3dbe73c4837b6916fcd4f7b147d7e482f2631399
Nathan Center <nathanjcenter@gmail.com>
2022-02-28 11:18:10 -0600

    added phase4

--- Program.cs
+++ Program.cs
@@ -7,17 +7,24 @@
 int secretNumber=42;
 // phase 3
 int tries=4;
-while (tries>0){
-    //phase 1
+int guessNumber=1;
+while (guessNumber<=tries){
+    //phase 4
+    Console.WriteLine($"Your guess - {guessNumber}");
+    //phase   1
+
     Console.WriteLine("User needs to guess the right number");
 int guess=int.Parse(Console.ReadLine());
 //phase 2
 if (guess==secretNumber){
     Console.WriteLine("You guess right!");
+    break;
 }
 else {
     Console.WriteLine("Sorry you are wrong!");
+    
 }
-tries--;
+//phase 4
+guessNumber++;
 }
 
